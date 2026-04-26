dadb3f9d599ad0662f74d3653eebc2e18133644e
Aya Alsaudi <ayaalsaudi6@gmail.com>
2026-04-26 11:48:25 +0300

    Solve Exercise 1 and 2

---
+++ script.js
@@ -0,0 +1,46 @@
+// Ex 1 :
+const input = [1, 2, 3];
+
+// Way 1: Using "for" Loop (The Manual Way) : 
+function solveWithFor(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(Math.pow(2, arr[i]));
+    }
+    return result;
+}
+console.log("For Result:", solveWithFor(input));
+
+
+// Way 2: Using "forEach" :
+function solveWithForEach(arr) {
+    let result = [];
+    arr.forEach(num => {
+        result.push(Math.pow(2, num));
+    });
+    return result;
+}
+console.log("ForEach Result:", solveWithForEach(input));
+
+
+// Way 3: Using "map" :
+function solveWithMap(arr) {
+    return arr.map(num => Math.pow(2, num));
+}
+console.log("Map Result:", solveWithMap(input));
+
+
+
+// Ex 2 :
+function checkEvenOdd(arr) {
+    return arr.map(element => {
+        if (typeof element !== 'number') {
+            return "N/A";
+        }
+        if (element % 2 === 0) {
+                return "even";
+        }
+        return "odd";
+        });
+}
+
